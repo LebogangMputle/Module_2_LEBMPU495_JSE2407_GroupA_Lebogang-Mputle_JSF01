7b7bc124bf799109365bb85c4b3f60c6c08bacfb
LebogangMputle <lebozikalala@gmail.com>
2024-07-18 10:00:54 +0200

    Added the sort, search and displayed my products title, ratings and price

--- my-new-app/src/app.js
+++ my-new-app/src/app.js
@@ -1,38 +1,38 @@
-// src/app.js
-import Alpine from 'alpinejs';
-import focus from '@alpinejs/focus';
-import persist from '@alpinejs/persist';
-import { getProducts, setupProductList } from './data/getProducts';
-import categories from './data/categories';
-import user from './data/userData';
-import cart from './components/cart';
-import toasts from './components/toasts';
+// // src/app.js
+// import Alpine from 'alpinejs';
+// import focus from '@alpinejs/focus';
+// import persist from '@alpinejs/persist';
+// import { getProducts, setupProductList } from './data/getProducts';
+// import categories from './data/categories';
+// import user from './data/userData';
+// import cart from './components/cart';
+// import toasts from './components/toasts';
 
-import { setupCounter } from './setupCounter';
+// import { setupCounter } from './setupCounter';
 
-window.Alpine = Alpine;
-Alpine.plugin(focus);
-Alpine.plugin(persist);
+// window.Alpine = Alpine;
+// Alpine.plugin(focus);
+// Alpine.plugin(persist);
 
-document.addEventListener('alpine:init', () => {
-   Alpine.data('categories', categories);
-   Alpine.data('user', user);
-   Alpine.data('cart', cart);
-   Alpine.data('toasts', toasts);
-});
+// document.addEventListener('alpine:init', () => {
+//    Alpine.data('categories', categories);
+//    Alpine.data('user', user);
+//    Alpine.data('cart', cart);
+//    Alpine.data('toasts', toasts);
+// });
 
-Alpine.start();
+// Alpine.start();
 
-document.addEventListener('DOMContentLoaded', async () => {
-  const productsData = await getProducts();
-  const productListElement = document.getElementById('product-list');
+// document.addEventListener('DOMContentLoaded', async () => {
+//   const productsData = await getProducts();
+//   const productListElement = document.getElementById('product-list');
   
-  if (productListElement) {
-    setupProductList(productsData, productListElement);
-  }
+//   if (productListElement) {
+//     setupProductList(productsData, productListElement);
+//   }
 
-  const counterElement = document.getElementById('counter');
-  if (counterElement) {
-    setupCounter(counterElement);
-  }
-});
+//   const counterElement = document.getElementById('counter');
+//   if (counterElement) {
+//     setupCounter(counterElement);
+//   }
+// });
